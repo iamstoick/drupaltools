34641b9731ebcde8b442b89ae2f18629f0dd88b1
Gerald Villorente <gerald.villorente@kitesystems.com>
2013-07-09 14:00:16 +0800

    Add description in the script file

--- exportDb.sh
+++ exportDb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Export database with date in filename.
+# Export database with date in filename and compress using gzip format. 
 
 drush sql-dump --result-file="backup_`date +%d-%m-%y`.sql" --gzip
