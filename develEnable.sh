396004c7c44b5a70b31d02f472c27667ca86eee5
Gerald Villorente <gerald.villorente@kitesystems.com>
2013-07-09 13:59:03 +0800

    Add description in the script file

--- develEnable.sh
+++ develEnable.sh
@@ -1,3 +1,5 @@
+# Enable devel module and set necessary permission.
+
 drush en devel -y
 drush sql-query "INSERT INTO role_permission (rid, permission, module) VALUES (3, 'access devel information', 'devel')"
 drush vset devel_query_display 0
