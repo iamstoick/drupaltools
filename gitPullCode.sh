cb602c598d7739b09d3055fe8c1a8743df91f18d
Gerald Villorente <gerald.villorente@kitesystems.com>
2013-08-11 15:52:17 +0800

    Added comment

--- gitPullCode.sh
+++ gitPullCode.sh
@@ -4,5 +4,6 @@ BRANCH_NAME=$(git symbolic-ref -q HEAD)
 BRANCH_NAME=${BRANCH_NAME##refs/heads/}
 BRANCH_NAME=${BRANCH_NAME:-HEAD}
 
+# Replace the 'origin' and 'acquia' with your repo alias(es).
 git pull acquia $BRANCH_NAME
 git pull origin $BRANCH_NAME
