91206fa939e85ad2e810cea563c3944f687267ee
Gerald Villorente <geraldvillorente@gmail.com>
2013-09-30 10:42:32 +0800

    Add licensing agreement.

--- install-drupal.sh
+++ install-drupal.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
-
-# A utility script to lessen the effort of setting up of Drupal.
 #
-# Written by Gerald Villorente.
+# install-drupal.sh
+# 
+# Copyright 2013 Gerald Villorente <geraldvillorente@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# About this script:
+#
+# A utility script to lessen the effort of setting up of Drupal.
 #
 # This script can be use to directly download the Drupal package.
 # The Drupal version can be specify by specifying the Drupal version in the
@@ -13,7 +32,7 @@
 # This script requires Drush.
 #
 # Note: This is not tested on Drupal 8 yet. Use this script with extra care.
-
+#
 # Help
 usage() {
   echo ""
@@ -33,10 +52,10 @@ usage() {
   echo "  ./install-drupal.sh myportfolio /home/gerald/html/ drupal-6       Download latest recommended release of Drupal 6.x."
   exit 1
 }
-
+#
 # Get the scripts root directory.
 SCRIPTSDIR=$(pwd)
-
+#
 # Validate the first parameter.
 if [ -z "$1" ]; then
   echo "Please check your first parameter, it appears to be empty. Read the guide using ./install-drupal.sh --help"
@@ -50,7 +69,7 @@ else
     PROJECT=$1
   fi
 fi
-
+#
 # Validate second parameter.
 if [ -z "$2" ]; then
   echo "Second parameter is required. Read the guide using ./install-drupal.sh --help"
@@ -64,7 +83,7 @@ else
     exit 1
   fi
 fi
-
+#
 # Validate the third parameter.
 if [ -z "$3" ]; then
   echo "Please check your second parameter, it appears to be empty. Read the guide using ./install-drupal.sh --help"
@@ -84,27 +103,27 @@ else
     exit 1
   fi
 fi
-
+#
 # Navigate to the target directory.
 cd $TARGETPATH
-
+#
 # Get the Drupal package.
 echo "Downloading Drupal package. This may take several seconds or minutes depending on your internet speed..."
 drush dl $VERSION
-
+#
 # Rename the directory.
 LATESTDIR=$(ls -t . | head -1)
 mv $LATESTDIR $PROJECT
-
+#
 # Update the permission.
 #chmod 755 -R $PROJECT
 # Create file directory.
 #mkdir -p $PROJECT/sites/default/files
 #chmod 777 -R $PROJECT/sites/default/files
-
+#
 # Navigate inside Drupal directory.
 cd $PROJECT
-
+#
 # Setup process.
 echo -n "Would you like to continue[Yes/No]: "
 read CONTINUE
@@ -123,7 +142,7 @@ if [ "$CONTINUE" == "Yes" ] || [ "$CONTINUE" == "Y" ] || [ "$CONTINUE" == "y" ];
   echo -n "We have to create a new database. Please specify the name: "
   read DBNAME
   mysqladmin -u $DBUSERNAME -p$DBPASSWORD create $DBNAME
-
+  #
   echo ""
   echo -n "Enter the admin email: "
   read DRUPALADMINEMAIL
@@ -140,10 +159,10 @@ if [ "$CONTINUE" == "Yes" ] || [ "$CONTINUE" == "Y" ] || [ "$CONTINUE" == "y" ];
   else
     PROFILE="standard"
   fi
-
+  #
   # Run the installer.
   drush site-install $PROFILE --db-url=mysql://$DBUSERNAME:$DBPASSWORD@localhost:$DBPORT/$DBNAME --account-mail=$DRUPALADMINEMAIL --account-name=$DRUPALUSERNAME --account-pass=$DRUPALPASSWORD --site-name=$DRUPALSITENAME --site-mail=$SITEEMAIL
-
+  #
   echo "Installation done."
   echo ""
   echo -n "Would you like to create new virtual host entry[Yes/No]: "
